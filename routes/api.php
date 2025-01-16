e4c0b1ad5731e8eba058115698e0748939a896d4
azizovmuhammadali <alico.777777@gmail.com>
2025-01-16 17:41:54 +0500

    Added book routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::apiResource('books',BookController::class)->middleware('lang');
