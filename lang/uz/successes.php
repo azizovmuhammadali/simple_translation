9a97d2b3d9ea53ee97f807d3c9bf8a5a6707d1be
azizovmuhammadali <alico.777777@gmail.com>
2025-01-16 17:41:43 +0500

    Success responses in uzbek

---
+++ lang/uz/successes.php
@@ -0,0 +1,8 @@
+<?php 
+return [
+    "all"=> "Hamma kitoblar",
+    "created"=> "Kitob yaratildi",
+    "show"=> "Siz izlagan kitob",
+    "updated"=> "Kitob yangilandi",
+    "deleted"=> "Kitob o\'chirildi",
+];
