0885714090b0ea739da03399bfd8d9dc8b4caa12
azizovmuhammadali <alico.777777@gmail.com>
2025-01-16 17:38:22 +0500

    Middleware for multiple languages

---
+++ app/Http/Middleware/SetlocaleMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetlocaleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $locale = $request->header('Accept-Language');
+
+        // Faqat qo'llab-quvvatlanadigan tillarni qabul qilish
+        if (in_array($locale, ['uz', 'en', 'ru'])) {
+            app()->setLocale($locale);
+        } else {
+            app()->setLocale('en'); // Default til
+        }
+
+        return $next($request);
+    }
+}
